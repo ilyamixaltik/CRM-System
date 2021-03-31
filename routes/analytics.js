c599be7ec19541fc8959781efdb26124e6e33d9f
ilyamixaltik <mixaltik16@gmail.com>
2021-03-31 19:24:12 +0300

    fix analytics router

--- routes/analytics.js
+++ routes/analytics.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const controller = require("../controllers/analytics")
 
 router.get('/overview', controller.overview)
-router.get('/analytics', controller.register)
+router.get('/analytics', controller.analytics)
 
 module.exports = router
