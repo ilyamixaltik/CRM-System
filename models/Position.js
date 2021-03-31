ecb3d054120da383089a19eb5d881b41be89997a
ilyamixaltik <mixaltik16@gmail.com>
2021-03-31 20:16:05 +0300

    create positions models

---
+++ models/Position.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+let positionSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    cost: { type: Number, required: true },
+    category: { ref: 'categories', type: mongoose.Schema.Types.ObjectId },
+    user: { ref: 'users', type: mongoose.Schema.Types.ObjectId }
+});
+
+module.exports = mongoose.model('positions', positionSchema);
