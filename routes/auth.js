f98627cb8766c467f118da05ab003d0460387eb3
ilyamixaltik <mixaltik16@gmail.com>
2021-03-31 18:28:12 +0300

    create auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const controller = require("../controllers/auth")
 
-router.get('/login', (req, res) => {
-    res.status(200).json({
-        login: true
-    })
-})
+router.get('/login', controller.login)
 
 module.exports = router
