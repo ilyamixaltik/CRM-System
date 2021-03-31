515239cb57698501e61cf4531a46fc9314d8dc2d
ilyamixaltik <mixaltik16@gmail.com>
2021-03-31 19:58:45 +0300

    add body parser lib

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 const analyticsRoutes = require("./routes/analytics")
 const authRoutes = require("./routes/auth")
 const categoryRoutes = require("./routes/category")
 const orderRoutes = require("./routes/order")
 const positionRoutes = require("./routes/position")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/auth', authRoutes)
