d5ec5500a9c256aa4b1638f4e0b709d5c9762539
ilyamixaltik <mixaltik16@gmail.com>
2021-03-31 18:24:50 +0300

    create auth routes

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+    res.status(200).json({
+        login: true
+    })
+})
+
+module.exports = router
