d9996657c0d3e6332c638f3b23f856ba77e56865
ilyamixaltik <mixaltik16@gmail.com>
2021-04-01 17:43:56 +0300

    create logic get by category id

--- controllers/position.js
+++ controllers/position.js
@@ -1,5 +1,18 @@
-module.exports.getByCategoryId = (req, res) => {
+const Position = require("../models/Position")
+const errorHandler = require("../utils/errorHandler")
 
+module.exports.getByCategoryId = async (req, res) => {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+
+        res.status(200).json(positions)
+        
+    } catch (err) {
+        errorHandler(res, err)
+    }
 }
 
 module.exports.create = (req, res) => {
