8723c8363e6ed27c9e798ec95487bd991a3a1751
ilyamixaltik <mixaltik16@gmail.com>
2021-04-01 17:52:46 +0300

    add logic create in position

--- controllers/position.js
+++ controllers/position.js
@@ -9,14 +9,26 @@ module.exports.getByCategoryId = async (req, res) => {
         })
 
         res.status(200).json(positions)
-        
+
     } catch (err) {
         errorHandler(res, err)
     }
 }
 
-module.exports.create = (req, res) => {
-    
+module.exports.create = async (req, res) => {
+    try {
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save()
+
+        res.status(201).json(position)
+        
+    } catch (err) {
+        errorHandler(res, err)
+    }
 }
 
 module.exports.remove = (req, res) => {
