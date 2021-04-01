63f446046ca871b8f7bc2706903ca098faa11bb2
ilyamixaltik <mixaltik16@gmail.com>
2021-04-01 13:51:12 +0300

    create register logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("./models/User")
+
 module.exports.login = (req, res) => {
     res.status(200).json({
         login: { 
@@ -7,8 +9,11 @@ module.exports.login = (req, res) => {
     })
 }
 
-module.exports.register = (req, res) => {
-    res.status(200).json({
-        register: 'from controller'
+module.exports.register = async (req, res) => {
+    const $user = new User({
+        email: req.body.email,
+        password: req.body.password
     })
+
+    await $user.save()
 }
