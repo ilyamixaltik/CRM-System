6b393981382702171ff343875b4ca3e215bb98a4
ilyamixaltik <mixaltik16@gmail.com>
2021-04-02 17:33:09 +0300

    add defence in category routers

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,14 @@
 const express = require("express")
+const passport = require("passport")
 const router = express.Router()
 
 const upload = require("../middleware/upload")
 const controller = require("../controllers/category")
 
-router.get('/', controller.getAll)
-router.get('/:id', controller.getById)
-router.delete('/:id', controller.remove)
-router.post('/', upload.single('image'), controller.create)
-router.patch('/:id', upload.single('image'), controller.update)
+router.get('/', passport.authenticate('jwt', { session: false }), controller.getAll)
+router.get('/:id', passport.authenticate('jwt', { session: false }), controller.getById)
+router.delete('/:id', passport.authenticate('jwt', { session: false }), controller.remove)
+router.post('/', passport.authenticate('jwt', { session: false }), upload.single('image'), controller.create)
+router.patch('/:id', passport.authenticate('jwt', { session: false }), upload.single('image'), controller.update)
 
 module.exports = router
