6461e83ec11c09907c16f7ccfa69bb482ad3e6ff
ilyamixaltik <mixaltik16@gmail.com>
2021-04-02 16:42:09 +0300

    add logic remove in category

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/Category")
+const Position = require("../models/Position")
 const errorHandler = require("../utils/errorHandler")
 
 module.exports.getAll = async (req, res) => {
@@ -15,14 +16,24 @@ module.exports.getById = async (req, res) => {
     try {
         const category = await Category.findById(req.params.id)
         res.status(200).json(category)
-        
+
     } catch (err) {
         errorHandler(res, err)
     }
 }
 
-module.exports.remove = (req, res) => {
-    
+module.exports.remove = async (req, res) => {
+    try {
+        await Category.remove({ _id: req.params.id })
+        await Position.remove({ category: req.params.id })
+
+        res.status(202).json({
+            message: 'Категория успешно удалена'
+        })
+
+    } catch (err) {
+        errorHandler(res, err)
+    }
 }
 
 module.exports.create = (req, res) => {
