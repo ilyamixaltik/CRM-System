a678d8f33966fa3ad28650273ffae89d20f10a9d
ilyamixaltik <mixaltik16@gmail.com>
2021-04-02 16:34:21 +0300

    add logic get all in category

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,14 @@
-module.exports.getAll = (req, res) => {
+const Category = require("../models/Category")
+const errorHandler = require("../utils/errorHandler")
 
+module.exports.getAll = async (req, res) => {
+    try {
+        const categories = await Category.find({ user: req.user.id })
+        res.status(200).json(categories)
+        
+    } catch (err) {
+        errorHandler(res, err)
+    }
 }
 
 module.exports.getById = (req, res) => {
