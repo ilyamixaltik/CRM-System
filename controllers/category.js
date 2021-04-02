b84856e23ed4c27fa02cd0264122f00eb9714f99
ilyamixaltik <mixaltik16@gmail.com>
2021-04-02 16:37:10 +0300

    add logic get by id in category

--- controllers/category.js
+++ controllers/category.js
@@ -5,14 +5,20 @@ module.exports.getAll = async (req, res) => {
     try {
         const categories = await Category.find({ user: req.user.id })
         res.status(200).json(categories)
-        
+
     } catch (err) {
         errorHandler(res, err)
     }
 }
 
-module.exports.getById = (req, res) => {
-    
+module.exports.getById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id)
+        res.status(200).json(category)
+        
+    } catch (err) {
+        errorHandler(res, err)
+    }
 }
 
 module.exports.remove = (req, res) => {
