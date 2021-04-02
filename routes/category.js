34646c532d659c96e76d85762fabfce3d53c0c5a
ilyamixaltik <mixaltik16@gmail.com>
2021-04-02 17:12:32 +0300

    connect upload images in router category

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
+
+const upload = require("../middleware/upload")
 const controller = require("../controllers/category")
 
 router.get('/', controller.getAll)
 router.get('/:id', controller.getById)
 router.delete('/:id', controller.remove)
-router.post('/', controller.create)
-router.patch('/:id', controller.update)
+router.post('/', upload.single('image'), controller.create)
+router.patch('/:id', upload.single('image'), controller.update)
 
 module.exports = router
