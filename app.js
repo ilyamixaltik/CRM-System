e0cd8d3577aa3e3a92cd11ae97a80aadea391d51
ilyamixaltik <mixaltik16@gmail.com>
2021-04-02 17:40:55 +0300

    set uploads catalog in static

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(passport.initialize())
 require("./middleware/passport")(passport)
 
 app.use(require("morgan")('dev'))
+app.use('/uploads', express.static('uploads'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(require("cors")())
