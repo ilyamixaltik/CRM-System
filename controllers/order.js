e70e55bdf94cc25b8fc4b5e2fcc23317b67478f4
ilyamixaltik <mixaltik16@gmail.com>
2021-04-03 17:40:55 +0300

    add create method in order controller

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,24 @@
+const Order = require("../models/Order")
+const errorHandler = require("../utils/errorHandler")
+
 module.exports.getAll = (req, res) => {
 
 }
 
-module.exports.create = (req, res) => {
-    
+module.exports.create = async (req, res) => {
+    try {
+        const lastOrder = await Order.findOne({ _id: req.user.id }).sort({ date: -1 })
+        const maxOrder = lastOrder ? lastOrder.order : 0
+
+        const order = await new Order({
+            order: maxOrder + 1,
+            list: req.body.list,
+            user: req.user.id
+        }).save()
+
+        res.status(201).json(order)
+
+    } catch (err) {
+        errorHandler(res, err)
+    }
 }
